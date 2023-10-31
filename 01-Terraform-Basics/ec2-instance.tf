f7d12ac58bb91cca953d51c62ab5a72f7649e3a3
aamir <aamir@pop-os.localdomain>
2023-11-01 00:08:06 +0530

    Terraform ec2-instance manifest file added

---
+++ 01-Terraform-Basics/ec2-instance.tf
@@ -0,0 +1,25 @@
+# Terraform Settings Block
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~>5.23"
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  profile = "default"
+  region = var.region
+}
+
+# Resource Block
+resource "aws_instance" "ec2test" {
+  ami = var.ami
+  instance_type = var.instance_type
+
+  tags = {
+    Name = var.instance_name_tag
+  }
+}
